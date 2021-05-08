1a5750c9fd58380e00cbf2fd4f21f3e868e91aa4
RAJESH PRAJAPAT <rajeshkiitprajapati@outlook.com>
2021-05-08 20:42:49 +0530

    First

---
+++ main.js
@@ -0,0 +1,21 @@
+// $(document).ready(function () {
+    
+//     $("#menu1").on("click", function () {
+//         $(".rotate").toggleClass("flip");
+//     });
+    
+//     $("#menu1").click(function () {
+//         $(".li_one")
+//         .fadeIn()
+//         .addClass("animate__animated animate__bounceInDown animation-delay-100");
+//         $(".li_two")
+//         .fadeIn()
+//         .addClass("animate__animated animate__bounceInDown animation-delay-200");
+//         $(".li_three")
+//         .fadeIn()
+//         .addClass("animate__animated animate__bounceInDown animation-delay-300");
+//         $(".li_four")
+//         .fadeIn()
+//         .addClass("animate__animated animate__bounceInDown animation-delay-400");
+//     });
+// });
